eca90c69eebd6876c9766e7f92d36b403c68926f
samuel luka <samluka99@gmail.com>
2017-11-06 10:22:38 +0300

    finished the carousel section

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
-$(function($){
-  $( '.menu-btn').click(function () {
-  $( '.responsive-menu').toggle()  
-  })
-})
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+var slideIndex = 1;
+showSpans(slideIndex);
+
+function plusDivs(n) {
+  showSpans(slideIndex += n);
+}
+function showSpans(n) {
+  var i;
+  var s =
+  document.getElementsByClassName("slides");
+  if (n > s.length)
+  {slideIndex = 1}
+  if (n < 1)
+  { slideIndex = s.length} ;
+  for (var i = 0; i < s.length; i++) {
+    s[i]
+  }
+
+}
